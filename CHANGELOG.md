1a9c805a5f7796846269adfaa9caf15bf2545102
Colin Campbell <colin.campbell@gmail.com>
2022-01-01 23:06:22 -0800

    Adding bits for hex package

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.1.0 (01.01.2022)
+
+- Initial alpha release
--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule TablerIcons.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/ColinCampbell/ex_tabler_icons"
+  @version "0.1.0"
+
   def project do
     [
       app: :ex_tabler_icons,
-      version: "0.1.0",
+      name: "ExTablerIcons",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs(),
+      source_url: @source_url
     ]
   end
 
@@ -19,7 +27,33 @@ defmodule TablerIcons.MixProject do
 
   defp deps do
     [
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    """
+    Easily integrate tabler icons into your Elixir apps.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Colin Campbell"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "ExTablerIcons",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/ex_tabler_icons",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE"]
     ]
   end
 end
